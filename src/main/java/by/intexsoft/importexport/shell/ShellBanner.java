0aaa6d823027fd3f065b911ab28dd637eba09776
sergey.buzuk <sergey.buzuk@intexsoft.by>
2018-12-12 13:49:14 +0300

    add new banner to shell

--- src/main/java/by/intexsoft/importexport/shell/ShellBanner.java
+++ src/main/java/by/intexsoft/importexport/shell/ShellBanner.java
@@ -3,21 +3,22 @@ package by.intexsoft.importexport.shell;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.shell.plugin.support.DefaultBannerProvider;
+import org.springframework.shell.support.util.FileUtils;
 import org.springframework.shell.support.util.OsUtils;
 import org.springframework.stereotype.Component;
 
+/**
+ * {@inheritDoc}
+ */
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ShellBanner extends DefaultBannerProvider {
-    public String getBanner(){
-        return  "=======================================" +
-                OsUtils.LINE_SEPARATOR +
-                "*        Import/Export events         *" +
-                OsUtils.LINE_SEPARATOR +
-                "=======================================" +
-                OsUtils.LINE_SEPARATOR +
-                "Version:" +
-                this.getVersion();
+    public String getBanner() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(FileUtils.readBanner(this.getClass(), "/shell_banner.txt"));
+        sb.append(OsUtils.LINE_SEPARATOR);
+        sb.append(getVersion()).append(OsUtils.LINE_SEPARATOR);
+        return sb.toString();
     }
 
     public String getVersion() {
